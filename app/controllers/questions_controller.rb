aab1be313ab9c72f16674bbed77e04cf7b043879
Maria Thorson <maria.thorson7@gmail.com>
2023-08-19 22:30:38 +0200

    rails stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = "I don't care, get dressed and go to work!"
+    @answer = "Silly question, get dressed and go to work!" if @question.include?("?")
+    @answer = "Great!" if @question == "I am going to work"
+  end
+
+
+
+end
